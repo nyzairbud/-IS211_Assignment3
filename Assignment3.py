150149cbdb365a1b5a3611c8f748c93ade188024
nyzairbud <alasgarcia27@gmail.com>
2018-02-17 21:19:15 -0500

    work in progress

---
+++ Assignment3.py
@@ -0,0 +1,23 @@
+from __future__ import print_function, division
+
+import urllib2
+import datetime
+import logging
+import argparse
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+_logger = logging.getLogger('assignment3')
+
+URL = "http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv"
+def Download(info):
+    ans = urllib2.urlopen(info)
+    return ans.read()
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='fetches the url')
+    parser.add_argument('--info', '-u', '-url', help='tells you the url')
+    args = parser.parse_args()
+    _logger.info("Using URL = {}".format(args.url))
