030f1f78c385b8f4226886c980c95f2c9369f055
nyzairbud <alasgarcia27@gmail.com>
2018-02-19 19:06:47 -0500

    work in progress

--- Assignment3.py
+++ Assignment3.py
@@ -6,6 +6,7 @@ import logging
 import argparse
 import re
 import datetime
+import operator
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 _logger = logging.getLogger('assignment3')
@@ -26,15 +27,39 @@ def csv_data(content):
     firefox = 0
     other = 0
     times = {i: 0 for i in range(0, 24)}
-    for row in csv_data:
+
+    for row in csv_content:
         result = {"path": row[0], "date": row[1], "browser": row[2], "status": row[3], "size": row[4]}
 
         date = datetime.datetime.strptime(result["date"], date_results)
         times[date.hour] = times[date.hour] + 1
 
+        hits += 1
+        if re.search(r"\.(?:jpg|jpeg|gif|png)$", result["path"], re.I | re.M):
+            imgHits += 1
+
+        elif re.search("chrome/\d+", result["browser"], re.I):
+            chrome += 1
+
+        elif re.search("safari", result["browser"], re.I) and not re.search("chrome/\d+", result["browser"], re.I):
+            safari += 1
+
+        elif re.search("firefox", result["browser"], re.I):
+            firefox += 1
+
+        elif re.search("other", result["browser"], re.I):
+            other += 1
+    imageRequest = (float(imgHits) / hits) * 100
+    browsers = {"Safari": safari, "Chrome": chrome, "Firefox": firefox, "OTHER": other}
+
+    print "Results are as shown:"
+    print "Image requests account for {0:0.1f}% of all requests.".format(imageRequest)
+    print "The most popular browser is %s." % (max(browsers.iteritems(), key=operator.itemgetter(1))[0])
 
 def main():
     parser = argparse.ArgumentParser(description='fetches the url')
     parser.add_argument('--info', '-u', '-url', help='tells you the url')
     args = parser.parse_args()
     _logger.info("Using URL = {}".format(args.info))
+
+    data = Download(args.info)
